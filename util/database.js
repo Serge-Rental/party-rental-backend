5bf2533f5f0ecb4574def375d85c88778d0a4c0e
sergethi <nsergearistide@gmail.com>
2023-11-07 14:24:43 -0500

    added db url for production

--- util/database.js
+++ util/database.js
@@ -1,16 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const sequelize = new Sequelize
-    (`postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@localhost:5432/${process.env.PG_DB}`, { logging: false})
-    // process.env.PG_DB,
-    // process.env.PG_USER,
-    // process.env.PG_PASSWORD,
-    // {
-    //     host: process.env.PG_HOST,
-    //     dialect: 'postgres',
-    // }
-
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@localhost:5432/${process.env.PG_DB}`,
+  { logging: false }
+);
+// process.env.PG_DB,
+// process.env.PG_USER,
+// process.env.PG_PASSWORD,
+// {
+//     host: process.env.PG_HOST,
+//     dialect: 'postgres',
+// }
 
 module.exports = sequelize;
